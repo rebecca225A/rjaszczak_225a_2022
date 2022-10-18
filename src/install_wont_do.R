84f1dcee016f2c978a8849e80fc76c906527f129
rebecca225A <113630598+rebecca225A@users.noreply.github.com>
2022-10-17 23:34:13 -0700

    workshop 4 code

---
+++ src/install_wont_do.R
@@ -0,0 +1,7 @@
+install.packages('caret')
+BiocManager::install("MAST")
+install.packages("lifecycle")
+install.packages('ranger')
+install.packages('devtools')
+devtools::install_github("vqv/ggbiplot")
+devtools::install_github("enriquea/feseR")
